1132e283261f4367ff34112371eaecbe4779d36a
cogitoErgoSum <2216155077@qq.com>
2022-05-25 11:01:49 +0800

    Update AliOss.py

--- alioss/AliOss.py
+++ alioss/AliOss.py
@@ -6,7 +6,7 @@ import uuid
 class AliOss:
     def __init__(self):
         # 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        self.auth = oss2.Auth('LTAI5t6Sa6Z2fXFigPLcBYe6', 'EcMOd2Q4mFiUWCqC128XxZHjZugL1V')
+        self.auth = oss2.Auth('Key', 'Id')
         # yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
         self.endpoint = 'oss-cn-shanghai.aliyuncs.com'
         # 填写Bucket名称，例如examplebucket。
